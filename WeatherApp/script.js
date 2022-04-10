36b269fd3891f115b9dd530f4062b4e090e20020
Mitesh Mayavanshi <82865550+Mitesh-1710@users.noreply.github.com>
2022-04-10 21:23:08 +0530

    Add files via upload

---
+++ WeatherApp/script.js
@@ -0,0 +1,61 @@
+let weather = {
+
+    'apiKey' : '68c56902e46172358540ee099d8fe48b',
+    fetchWeather : function(city){
+        
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}&units=metric`).then((response) => response.json()).then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const name = data.name;
+        const {icon , description} = data.weather[0];
+        const {temp} = data.main;
+        const speed = data.wind.speed;
+      
+        document.querySelector(".city").innerHTML = `Weather in ${name}`;
+        document.querySelector('.icon').src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+        document.querySelector(".description").innerHTML = description;
+        document.querySelector(".temp").innerHTML = temp + "°C";
+        document.querySelector(".weather").classList.remove("loading");
+        document.querySelector(".wind").innerHTML =
+        "Wind Speed : " + speed + " km/h";
+        if(description == "clear sky")
+        {
+            document.querySelector('.container').style.backgroundImage = "url('clearsky.jpg')";
+        }
+        else if(description == "few clouds" || "scattered clouds" )
+        {
+            document.querySelector('.container').style.backgroundImage = "url('cloudy.jpg')";
+        }
+        else if(description == "shower rain" || "rain" )
+        {
+            document.querySelector('.container').style.backgroundImage = "url('rain.jpg')";
+        }
+        else if(description == "thunderstorm" )
+        {
+            document.querySelector('.container').style.backgroundImage = "url('thunderstorm.JPG')";
+        }
+        else if(description == "snow")
+        {
+            document.querySelector('.container').style.backgroundImage = "url('snow.jpg')";
+        }
+      
+    },
+    search : function(){
+        this.fetchWeather(document.querySelector('.search-bar').value);
+    },
+    
+};
+
+document.querySelector('.search-btn').addEventListener('click',function(){
+
+    weather.search();
+
+});
+
+document.querySelector('.search-bar').addEventListener('keyup',function(event){
+    if(event.key == "Enter")
+    {
+        weather.search();
+    }
+})
+
